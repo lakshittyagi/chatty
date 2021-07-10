04b3d7d10042a8f76049c14ef67eb00fc825e18d
lakshit <akshutyagi2016@gmail.com>
2021-07-11 02:21:10 +0530

    login system added to the chat application

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-const app = require('express')();
+const express = require("express");
+const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const path = require("path");
 const mongoose = require('mongoose');
+var cors = require('cors')
+const { body, validationResult } = require('express-validator');
 
+var corsOptions = {
+    origin: 'http://localhost:3000/*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+// app.use(app.use(cors(corsOptions)));
+app.use(express.urlencoded());
+app.use(express.json());
 mongoose.connect('mongodb://localhost/chatty')
     .then(() => console.log('Connected…'))
     .catch(err => console.error('Connection failed…'));
+
+
+
 
 
 // Defning a schema
@@ -55,6 +68,24 @@ app.get("/", (request, response) => {
 app.get("/login", (request, response) => {
     response.sendFile(path.join(__dirname, "./src/login.html"));
 });
+
+app.post("/login", cors(corsOptions), (request, response) => {
+    // console.log(request.body);
+    const name = request.body.name;
+    Data(name, request, response);
+});
+
+async function Data(name, request, response) {
+    console.log(name);
+    // return response.send(name);
+    let user = await User.findOne({ 'name': name });
+    if (!user) {
+        user = new User({ 'name': name, 'uuid': new Date().getTime() });
+        user = await user.save();
+    }
+    console.log(user);
+    return response.json(user);
+}
 
 
 
